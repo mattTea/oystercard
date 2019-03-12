f79ffb3faefc012603a6484f19235f278818379a
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-03-12 18:19:40 +0000

    Adds readme structure

---
+++ README.md
@@ -0,0 +1,32 @@
+## Oystercard
+Description...
+
+
+## Motivation
+A short description of the motivation behind the creation and maintenance of the project. This should explain why the project exists.
+
+Project to learn and further practice the following concepts...
+
+- I write code that is easy to change
+- I can test-drive my code
+- I can build with objects
+
+
+## Tech/framework used
+- Ruby
+- Rspec
+
+## Features
+- ...
+
+
+## Installation
+Provide step by step series of examples and explanations about how to get a development env running.
+
+
+## Tests
+Describe and show how to run the tests with code examples.
+
+
+## How to use?
+Provide way to use the program
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,99 +1,94 @@
 require "oystercard"
 
 describe Oystercard do
-  before(:each) do
-    @card = Oystercard.new
-  end
+  let(:card) { Oystercard.new }
   
-  let(:in_station){ double :in_station }
-  let(:out_station){ double :out_station }
-
-  it { is_expected.to respond_to(:balance) } 
-  it { is_expected.to respond_to(:top_up) }
+  let(:in_station) { double :in_station }
+  let(:out_station) { double :out_station }
 
   it "returns a starting balance of 0" do
-    expect(@card.balance).to eq 0
+    expect(card.balance).to eq 0
   end
 
   it "should have a balance of 10 when user tops up 10" do
-    @card.top_up(10)
-    expect(@card.balance).to eq 10
+    card.top_up(10)
+    expect(card.balance).to eq 10
   end
 
   it "increases balance by top up amount" do 
-    @card.top_up(10)
-    @card.top_up(20)
-    expect(@card.balance).to eq 30
+    card.top_up(10)
+    card.top_up(20)
+    expect(card.balance).to eq 30
   end
   
   it "allows top up to 1 below limit amount" do
     limit = Oystercard::DEFAULT_LIMIT
-    expect(@card.top_up(limit - 1)).to eq limit - 1
+    expect(card.top_up(limit - 1)).to eq limit - 1
   end
 
   it "has a default limit of £90" do
     limit = Oystercard::DEFAULT_LIMIT
     error = "Top-up exceeds £#{limit} balance limit. Add lower amount."
-    @card.top_up(1)
-    expect { @card.top_up(limit) }.to raise_error error
+    card.top_up(1)
+    expect { card.top_up(limit) }.to raise_error error
   end
   
   it { is_expected.to respond_to(:touch_in) }
 
   it "should show in journey as false at first" do
-    expect(@card).to_not be_in_journey
+    expect(card).to_not be_in_journey
   end
 
   it "should change in journey status to true when card touched in" do
-    @card.top_up(5)
-    @card.touch_in(in_station)
-    expect(@card).to be_in_journey
+    card.top_up(5)
+    card.touch_in(in_station)
+    expect(card).to be_in_journey
   end
 
   it "should change in journey status to false when card touched out" do
-    @card.top_up(5)
-    @card.touch_in(in_station)
-    @card.touch_out(out_station)
-    expect(@card).to_not be_in_journey
+    card.top_up(5)
+    card.touch_in(in_station)
+    card.touch_out(out_station)
+    expect(card).to_not be_in_journey
   end
 
   it "should have a minimum balance for a single journey" do
-    expect { @card.touch_in(in_station) }.to raise_error "Insufficient funds"
+    expect { card.touch_in(in_station) }.to raise_error "Insufficient funds"
   end
 
   it "should deduct fare from balance" do
-    @card.top_up(5)
-    @card.touch_in(in_station)
+    card.top_up(5)
+    card.touch_in(in_station)
     fare = Oystercard::MINIMUM_FARE
 
-    expect {@card.touch_out(out_station)}.to change{@card.balance}.by(- fare)
+    expect {card.touch_out(out_station)}.to change{card.balance}.by(- fare)
   end
 
   it "should record journey entry station" do
-    @card.top_up(5)
-    expect {@card.touch_in(in_station)}.to change{@card.entry_station}.to(in_station)
+    card.top_up(5)
+    expect {card.touch_in(in_station)}.to change{card.entry_station}.to(in_station)
   end
 
   it "should set entry_station to nil on touch_out" do
-    @card.top_up(10)
-    @card.touch_in(in_station)
-    @card.touch_out(out_station)
-    expect(@card.entry_station).to eq nil
+    card.top_up(10)
+    card.touch_in(in_station)
+    card.touch_out(out_station)
+    expect(card.entry_station).to eq nil
   end
 
   it "should record journey exit station" do
-    @card.touch_out(out_station)
-    expect(@card.exit_station).to eq out_station
+    card.touch_out(out_station)
+    expect(card.exit_station).to eq out_station
   end
 
   it "should create a new card with an empty list of journeys" do
-    expect(@card.list_of_journeys).to eq []
+    expect(card.list_of_journeys).to eq []
   end
 
   it "should create a journey after touching in and out" do
-    @card.top_up(10)
-    @card.touch_in(in_station)
-    @card.touch_out(out_station)
-    expect(@card.list_of_journeys).to eq [{in_station => out_station}]
+    card.top_up(10)
+    card.touch_in(in_station)
+    card.touch_out(out_station)
+    expect(card.list_of_journeys).to eq [{in_station => out_station}]
   end
 end
